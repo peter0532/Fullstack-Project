5092a8233e1ff72d5e9520b2a590e7d6e80950f1
peter0532 <peteradamson82@gmai.com>
2024-02-22 18:48:19 +0100

    initial commit

---
+++ backend/config.js
@@ -0,0 +1,6 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://daniandredah:daniel100@books.vvrhv9s.mongodb.net/books?retryWrites=true&w=majority'
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
